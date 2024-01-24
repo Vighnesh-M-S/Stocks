3c5e9c8d2e28f3bf3498812bd71534c219cb322b
Vighnesh <princevighnesh71@gmail.com>
2024-01-24 18:49:01 +0530

    data.js

---
+++ stocks/src/data.js
@@ -0,0 +1,46 @@
+const data={
+    exchanges: [
+        {
+            name: 'TATA',
+            index: 841.42,
+            change: 1.54 
+        },
+        {
+            name: 'Reliance',
+            index: 841.42,
+            change: 1.54 
+        },
+        {
+            name: 'Maruthi',
+            index: 841.42,
+            change: 1.54 
+        },
+        {
+            name: 'Jio',
+            index: 841.42,
+            change: 1.54 
+        },
+    ],
+
+    stocks: [
+        {
+            name: "Viedek",
+            ticker: "VIE",
+            bid: 128.5,
+            ask:143.5,
+            sector: "Industry",
+            dividends:{
+                2018: 18,
+                2017: 15,
+                2016: 12,
+                2015: 11,
+            },
+            homepage: "https://google.com",
+            investorpage: "https://google.com"
+
+                
+            
+
+        }
+    ]
+}
