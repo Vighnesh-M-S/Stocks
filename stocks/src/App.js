884aa93ab9bda1cb3229a975b62a77fb65cf3812
Vighnesh <princevighnesh71@gmail.com>
2024-03-13 21:15:59 +0530

    addons

--- stocks/src/App.js
+++ stocks/src/App.js
@@ -1,16 +1,52 @@
-import React from "react";
-import Header from "./components/Header/Header";
-import Subheader from "/Users/vighneshms/Downloads/Stocks/stocks/src/components/Subheader/Subheader.jsx";
-import ExchangeList from "./components/ExchangeList/ExchangeList.jsx";
-import data from "./data.js";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import data from "./data";
+
+import Dashboard from "./pages/Dashboard/Dashboard";
+import StockView from "./pages/StockView/StockView";
 
 function App() {
+  const [exchanges, setExchanges] = useState([]);
+  const [stocks, setStocks] = useState([]);
+
+  const getData = () => {
+    const exchanges = data.exchanges;
+    const stocks = data.stocks;
+
+    return {
+      exchanges,
+      stocks,
+    };
+  };
+
+  useEffect(() => {
+    const { exchanges, stocks } = getData();
+
+    setExchanges(exchanges);
+    setStocks(stocks);
+  }, []);
+
   return (
     <div className="App">
-      <Header/>
-      <Subheader>
-        <ExchangeList exchanges={data.exchanges}/>
-      </Subheader>
+      <Router>
+        <Route
+          exact
+          path="/"
+          render={(props) => (
+            <Dashboard
+              exchanges={exchanges}
+              stocks={stocks}
+              setStocks={setStocks}
+              {...props}
+            />
+          )}
+        />
+        <Route
+          path="/stocks/:ticker"
+          render={(props) => <StockView {...props} stocks={stocks} />}
+        />
+      </Router>
     </div>
   );
 }
--- stocks/src/components/Subheader/Subheader.jsx
+++ stocks/src/components/Subheader/Subheader.jsx
@@ -1,16 +1,13 @@
 import React from "react";
+
 import styles from "./Subheader.module.css";
 
-
 const Subheader = (props) => {
-    return(
-    <div class ={styles.Subheader}>
-        <div class ={styles.container}>
-            {props.children}
-        </div>
-
+  return (
+    <div className={styles.subheader}>
+      <div className={styles.container}>{props.children}</div>
     </div>
-    );
+  );
 };
 
 export default Subheader;
